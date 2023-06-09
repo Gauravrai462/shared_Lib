c482c42fc141b8a06dee8629f53ae50d0f924bed
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-06-09 08:19:16 +0530

    Update java.groovy

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,1 +1,32 @@
-
+def call () {
+  
+  pipeline {
+  
+  agent any
+    
+    options { 
+            disableConcurrentBuilds() 
+        }
+    environment {
+    
+    }
+    stages {
+    
+      stage ('clean workspace') {
+        step {
+          cleanWS()
+        
+        }
+      }
+      
+      stage ('checkout') {
+      
+        step {
+         
+        }
+      }
+    
+    
+    }
+  }
+}
