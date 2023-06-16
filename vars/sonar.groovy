ce11eed6358b6ab69bd3cedc8f8ec55055655e1f
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-06-16 08:59:59 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,6 +1,14 @@
-def call(credentialsId){
+/*def call(credentialsId){
+  
   withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
   sh ' mvn sonar:sonar -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url="http://172.17.0.1:9000"'
   }
 
+}*/
+
+def call(credentialsId){
+
+    withSonarQubeEnv(sonarqube) {
+      sh 'mvn clean package sonar:sonar'
+    }
 }
