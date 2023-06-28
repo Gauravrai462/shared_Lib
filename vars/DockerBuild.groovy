694b804cdfe21b3a47a43608a376383e1b9e5bbb
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-06-28 09:01:55 +0530

    Update DockerBuild.groovy

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String project, String hubUser, String ImageTag) {
   sh '''
-  docker image build -t ${hubUser}/${project} .
+  docker image build --tag ${hubUser}/${project} .
   docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
    docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
   '''  
