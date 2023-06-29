5f6f0c3e6c03a8e0e4c1ad7eac0fd81b28c24979
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-06-29 10:01:05 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,18 @@
-def call(String project, String hubUser, String ImageTag) {
+/*def call(String project, String hubUser, String ImageTag) {
     sh '''
      //docker image build -t ${hubUser}:${project} .
      docker image build -t ${hubUser}:${project} . 
      docker image tag ${hubUser}:${project} ${hubUser}/${project}:${ImageTag}
      docker image tag ${hubUser}:${project} ${hubUser}/${project}:latest
    '''  
+}*/
+
+
+def call(String project, String ImageTag, String hubUser){
+    
+  sh """
+    docker image build -t ${hubUser}/${project} . 
+    docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+    docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+   """
 }
