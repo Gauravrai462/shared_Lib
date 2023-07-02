674d045e8996fed2c6907416fca17af2f5e809ac
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-02 15:19:34 +0530

    Update ECR.groovy

--- vars/ECR.groovy
+++ vars/ECR.groovy
@@ -71,8 +71,8 @@ def call (def PipelineParams){
      steps{
         sh """
           
-          export AWS_ACCESS_KEY_ID=${echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"'}
-          export AWS_SECRET_ACCESS_KEY=${echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"'}
+          export AWS_ACCESS_KEY_ID=$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
+          export AWS_SECRET_ACCESS_KEY=$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
           export AWS_DEFAULT_REGION=${AWS_REGION}
           aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${DOCKER_REGISTRY}
           docker build -t ${DOCKER_REGISTRY}/${DOCKER_TAG}:${IMAGE_VERSION} .
@@ -95,7 +95,7 @@ def call (def PipelineParams){
    stage('Push Docker Image') {
             steps {
               sh """
-                export AWS_ACCESS_KEY_ID=${echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"'}
+                export AWS_ACCESS_KEY_ID=$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
                 export AWS_SECRET_ACCESS_KEY=${echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"'}
                 export AWS_DEFAULT_REGION=${AWS_REGION}
                 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${DOCKER_REGISTRY}
