a98a393562d1f0f5b9d5a23f65448dcf71f9dd65
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-02 15:12:37 +0530

    Update ECR.groovy

--- vars/ECR.groovy
+++ vars/ECR.groovy
@@ -12,8 +12,8 @@ def call (def PipelineParams){
       DOCKER_TAG="${PipelineParams.DOCKER_TAG}"
       BUILD_NUMBER="${BUILD_NUMBER}"
       IMAGE_VERSION="v_${BUILD_NUMBER}"
-      AWS_ACCESS_KEY_ID="credentials('aws_credentials').AWS_ACCESS_KEY_ID"
-      AWS_SECRET_ACCESS_KEY="credentials('aws_credentials').AWS_SECRET_ACCESS_KEY"
+      AWS_ACCESS_KEY_ID="credentials{'aws_credentials'}.AWS_ACCESS_KEY_ID"
+      AWS_SECRET_ACCESS_KEY="credentials{'aws_credentials'}.AWS_SECRET_ACCESS_KEY"
       
         
     }
