4bcb0d1582295294034964d8af50b81c63659de6
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-06 07:53:19 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -49,7 +49,7 @@ def call(def PipelineParams) {
      steps{
        
         sh """
-            export AWS_ACCESS_KEY_ID=${AWS_SECRET_ACCESS_KEY}"
+            export AWS_ACCESS_KEY_ID=${AWS_SECRET_ACCESS_KEY}
             export AWS_SECRET_ACCESS_KEY==${AWS_SECRET_ACCESS_KEY}
             export AWS_DEFAULT_REGION=${REGION}
             aws s3 cp /var/lib/jenkins/workspace/${PROJECT_NAME} s3://${BUCKET_NAME} --region ${REGION}
