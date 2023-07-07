694f764da15c464149be042f9aa37ccdc9a7cd00
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-07 16:03:41 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -52,8 +52,6 @@ def call(def PipelineParams) {
             aws_credentials=$(aws sts assume-role --role-arn arn:aws:iam::685793358766:role/Jenkins_AWS_role --role-session-name "AWSCLI-Session" --output json)
             export AWS_ACCESS_KEY_ID=\$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
             export AWS_SECRET_ACCESS_KEY=\$(echo $aws_credentials|jq '.Credentials.SecretAccessKey'|tr -d '"')
-            export AWS_SESSION_TOKEN=\$(echo $aws_credentials|jq '.Credentials.SessionToken'|tr -d '"')
-
             aws s3 cp ${PROJECT_NAME} s3://${BUCKET_NAME} --region ${REGION}
             '''
            
