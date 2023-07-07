51d355a7f29f59a58eee1577dce21b1cb8c6b067
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-07 18:10:13 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -51,12 +51,12 @@ def call(def PipelineParams) {
      
    }*/
 
-   /*stage('upload_to_S3'){
+   stage('upload_to_S3'){
        steps{
        
             sh '''
             aws_credentials=$(aws sts assume-role --role-arn arn:aws:iam::685793358766:role/Jenkins_AWS_role --role-session-name "AWSCLI-Session" --output json)
-            withCredentials([file(credentialsId: 'aws-credentials', variable: 'AWS_CREDENTIALS_JSON')])
+            //withCredentials([file(credentialsId: 'aws-credentials', variable: 'AWS_CREDENTIALS_JSON')])
             export AWS_ACCESS_KEY_ID=\$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
             export AWS_SECRET_ACCESS_KEY=\$(echo $aws_credentials|jq '.Credentials.SecretAccessKey'|tr -d '"')
             aws s3 cp ${PROJECT_NAME} s3://${BUCKET_NAME} --region ${REGION}
@@ -64,7 +64,7 @@ def call(def PipelineParams) {
            
      }
      
-   }*/
+   }
 
 
     /*stage('Assume IAM Role') {
