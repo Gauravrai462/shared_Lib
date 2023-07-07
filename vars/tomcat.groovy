51276c5d9bb960d3d557026dfdae5e8409da9618
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-07 16:21:24 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -44,15 +44,20 @@ def call(def PipelineParams) {
      }
      
    }
+   /*stage('install aws cli') {
+        sh 'aws configure'
+        
+     
+   }*/
 
    stage('upload_to_S3'){
        steps{
        
             sh '''
-            aws_credentials=$(aws sts assume-role --role-arn arn:aws:iam::685793358766:role/Jenkins_AWS_role --role-session-name "AWSCLI-Session" --output json)
+            /*aws_credentials=$(aws sts assume-role --role-arn arn:aws:iam::685793358766:role/Jenkins_AWS_role --role-session-name "AWSCLI-Session" --output json)
             withCredentials([file(credentialsId: 'aws-credentials', variable: 'AWS_CREDENTIALS_JSON')])
             export AWS_ACCESS_KEY_ID=\$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
-            export AWS_SECRET_ACCESS_KEY=\$(echo $aws_credentials|jq '.Credentials.SecretAccessKey'|tr -d '"')
+            export AWS_SECRET_ACCESS_KEY=\$(echo $aws_credentials|jq '.Credentials.SecretAccessKey'|tr -d '"')*/
             aws s3 cp ${PROJECT_NAME} s3://${BUCKET_NAME} --region ${REGION}
             '''
            
