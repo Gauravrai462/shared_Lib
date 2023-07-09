dbc92206f43832720b367087ed9dd4c93dd758a2
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-09 12:59:45 +0530

    Update ECR.groovy

--- vars/ECR.groovy
+++ vars/ECR.groovy
@@ -93,14 +93,15 @@ def call (def PipelineParams){
 
    stage('Push Docker Image') {
             steps {
-              sh """
-                export AWS_ACCESS_KEY_ID=$(echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"')
-                export AWS_SECRET_ACCESS_KEY=${echo $aws_credentials|jq '.Credentials.AccessKeyId'|tr -d '"'}
+              sh '''
+                aws_credentials=$(aws sts assume-role --role-arn arn:aws:iam::685793358766:role/Jenkins_AWS_role --role-session-name "AWSCLI-Session" --output json ) 
+                export AWS_ACCESS_KEY_ID=$(echo $aws_credentials|jq '.Credentials.AccessKeyId')
+                export AWS_SECRET_ACCESS_KEY=${echo $aws_credentials|jq '.Credentials.AccessKeyId'}
                 export AWS_DEFAULT_REGION=${AWS_REGION}
                 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${DOCKER_REGISTRY}
             
                 docker push ${DOCKER_REGISTRY}/${DOCKER_TAG}:${IMAGE_VERSION}
-                """
+                '''
                 }
             }
         
