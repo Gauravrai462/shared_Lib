526a9e4159bad3f84080edf731d00d65044393ad
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-09 13:03:22 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -18,8 +18,8 @@ def call(def PipelineParams) {
       SONAR_TOKEN= "SONAR_AUTH_TOKEN"
       PROJECT_NAME="${PipelineParams.PROJECT_NAME}"
       REGION="${PipelineParams.REGION}"  
-      PATH="${PipelineParams.PATH}"
-      WAR="${PipelineParams.WAR}"
+      //PATH="${PipelineParams.PATH}"
+      //WAR="${PipelineParams.WAR}"
        
   }
    stages{
@@ -53,7 +53,7 @@ def call(def PipelineParams) {
             aws_credentials=$(aws sts assume-role --role-arn arn:aws:iam::685793358766:role/Jenkins_AWS_role --role-session-name "AWSCLI-Session" --output json ) 
             export AWS_ACCESS_KEY_ID=\$(echo $aws_credentials|jq '.Credentials.AccessKeyId')
             export AWS_SECRET_ACCESS_KEY=\$(echo $aws_credentials|jq '.Credentials.SecretAccessKey')
-            aws s3 cp target/${WAR} s3://${BUCKET_NAME} --region ${REGION}
+            aws s3 cp target/vprofile-v2.war s3://${BUCKET_NAME} --region ${REGION}
             '''
            
      }
