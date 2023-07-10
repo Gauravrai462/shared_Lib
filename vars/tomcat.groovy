9d7176edbcd85a41f81a7a6d1813b3a7b31d986c
Gauravrai462 <77716616+Gauravrai462@users.noreply.github.com>
2023-07-10 13:53:17 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -77,8 +77,8 @@ def call(def PipelineParams) {
 
       sh '''
        
-        scp -i master-key.pem /target/${FILE} ubuntu@43.204.24.104 /opt 
-        ssh -i master-key.pem ubuntu@43.204.24.104 'systemctl restart tomcat9
+        scp -i master-key.pem /var/lib/jenkins/workspace/JAVA ubuntu@43.204.24.104:/tmp 
+        ssh -i master-key.pem ubuntu@43.204.24.104 'systemctl restart tomcat9'
        '''
 
        
